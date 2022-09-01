a69773f25535f26d9ebb8647943a6c1d4202582e
eyal20202 <eyal20202@gmail.com>
2022-09-01 22:39:24 +0300

    fix name method bean

--- src/main/java/com/eyal/exam/DogAndCat/config/ConfigInternalApi.java
+++ src/main/java/com/eyal/exam/DogAndCat/config/ConfigInternalApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigInternalApi {
     @Bean
-    public RestTemplate getRestTemplate() {
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
