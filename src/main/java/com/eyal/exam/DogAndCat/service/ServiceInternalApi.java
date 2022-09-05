c6c5873f8e66464b8a1fa7242a7b5f3b9157c8a0
eyal20202 <eyal20202@gmail.com>
2022-09-05 19:06:06 +0300

    change body request to json

--- src/main/java/com/eyal/exam/DogAndCat/service/ServiceInternalApi.java
+++ src/main/java/com/eyal/exam/DogAndCat/service/ServiceInternalApi.java
@@ -11,16 +11,22 @@ import java.util.Arrays;
 
 public class ServiceInternalApi {
     @Autowired
+    RestTemplate restTemplate;
 
     public String runService(){
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.setBearerAuth("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJzMDJ0YXBMRG1JeTVtVXNLTDdDdDdtTHpPYWJJOFRHbngyUTJ0WnRobEZzODg2ekN1VyIsImp0aSI6IjBiMjE3OTVkMDE2ZGRiNTdjOTEzMGRiZDQ4NjcxOTRiOTc0Nzg5ZGI3MjdjMjQ3MDU1YzQ5YWMxNmJmZGM3YmE4N2U5NjZkYWJjMDVmMGEyIiwiaWF0IjoxNjYyMDUzMTAyLCJuYmYiOjE2NjIwNTMxMDIsImV4cCI6MTY2MjA1NjcwMiwic3ViIjoiIiwic2NvcGVzIjpbXX0.zQ14iLpI4W6AAPXvmzEZag7pfvxMoo54tXF_xN2NtAuNOuJv3l7MqstjMG8fPCZV96_fLV2-6QaW4khrCubs2iEbT0L0L57VVmJapYmmUvE2_hm4CrSGtIk-JKqFu4vYRWzhuC9ku5Hg9XvG86C4FVWdhlpbwmjYG8YM9wVxcIX0nrRYXK_x2xlPq4bHpP5yAGyZQjfSMJpnetX4ErpZPRkzb3iWPKCOV-PqI0H2v7eEVuVdPENDSHVd-Tb_0dP44qDRBJkiXMKypFdfxhCK3I8bXoFO1O08EqqaWp-98_YPuOljHWrcE7MugnIlZuKPht_82veSuVpPYjCPNsDYPg");
-        String body = "type=dog&age=baby";
-        HttpEntity<String> entity = new HttpEntity<String>(body,headers);
-
-        return restTemplate.exchange("http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();
+        String requestJson = "{\"type\":\"dog\"}";
+//        String body = "type=dog&age=baby";
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson,headers);
+        if(restTemplate == null){
+            System.out.println("restTemplate == null");
+        }else{
+            System.out.println("restTemplate != null");
+        }
+        return rt.exchange("http://localhost:8080/products", HttpMethod.GET, entity, String.class).getBody();
 
 
         /**
