78d1a4b1b15b03ff12dbaa5549d9e9b372f0d994
eyal20202 <eyal20202@gmail.com>
2022-09-06 00:02:47 +0300

    age required

--- src/main/java/com/eyal/exam/DogAndCat/CharacteristicsAnimal.java
+++ src/main/java/com/eyal/exam/DogAndCat/CharacteristicsAnimal.java
@@ -2,28 +2,25 @@ package com.eyal.exam.DogAndCat;
 
 import com.eyal.exam.DogAndCat.service.ServiceInternalApi;
 import com.google.gson.Gson;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CharacteristicsAnimal {
     ServiceInternalApi serviceInternalApi = new ServiceInternalApi();// BaseURL
+//    @GetMapping("/type/{type}")
+//    @ResponseBody
+//    public Animals getAnimalByType(@PathVariable String type) {
+//        System.out.println("getAnimalByType type= " + type);
+//        String result = serviceInternalApi.runService(type);
+//        System.out.println("CharacteristicsCat java getAnimalByAge result = "+ result);
+//        Gson gson = new Gson();
+//        Animals animals = gson.fromJson(result, Animals.class);
+//        System.out.println("animal = "+ gson.toJson(animals));
+//        return animals;
+//    }
     @GetMapping("/type/{type}")
     @ResponseBody
-    public Animals getAnimalByType(@PathVariable String type) {
-        System.out.println("getAnimalByType type= " + type);
-        String result = serviceInternalApi.runService(type);
-        System.out.println("CharacteristicsCat java getAnimalByAge result = "+ result);
-        Gson gson = new Gson();
-        Animals animals = gson.fromJson(result, Animals.class);
-        System.out.println("animal = "+ gson.toJson(animals));
-        return animals;
-    }
-    @GetMapping("/type/{type}/age/{age}")
-    @ResponseBody
-    public Animals getAnimalByAge(@PathVariable String age,@PathVariable String type) {
+    public Animals getAnimalByAge( @PathVariable String type,@RequestParam(required = false) String age) {
         System.out.println("getAnimalByAge age= " + age);
         String result = serviceInternalApi.runService(type,age);
         System.out.println("CharacteristicsCat java getAnimalByAge result = "+ result);
