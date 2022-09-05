94c1bd2441f52a529afa94da495f0ef574ffd8f4
eyal20202 <eyal20202@gmail.com>
2022-09-05 19:23:21 +0300

    update token

--- src/main/java/com/eyal/exam/DogAndCat/config/ConfigInternalApi.java
+++ src/main/java/com/eyal/exam/DogAndCat/config/ConfigInternalApi.java
@@ -1,10 +1,10 @@
 package com.eyal.exam.DogAndCat.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@Configurable
 public class ConfigInternalApi {
     @Bean
     public RestTemplate restTemplate() {
