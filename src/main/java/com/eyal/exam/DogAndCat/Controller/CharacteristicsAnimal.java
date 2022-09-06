72ac81a7b6402dc194aee5b4b240002a1963c82d
eyal20202 <eyal20202@gmail.com>
2022-09-06 20:04:44 +0300

    update token

--- src/main/java/com/eyal/exam/DogAndCat/Controller/CharacteristicsAnimal.java
+++ src/main/java/com/eyal/exam/DogAndCat/Controller/CharacteristicsAnimal.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class CharacteristicsAnimal {
     ServiceInternalApi serviceInternalApi = new ServiceInternalApi();// BaseURL
