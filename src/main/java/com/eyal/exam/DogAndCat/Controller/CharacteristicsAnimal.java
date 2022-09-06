2a33e9d93dc0863515c1cd050e6d2063778884b2
eyal20202 <eyal20202@gmail.com>
2022-09-06 17:56:01 +0300

    update token

--- src/main/java/com/eyal/exam/DogAndCat/Controller/CharacteristicsAnimal.java
+++ src/main/java/com/eyal/exam/DogAndCat/Controller/CharacteristicsAnimal.java
@@ -13,18 +13,19 @@ import org.springframework.web.bind.annotation.*;
 public class CharacteristicsAnimal {
     ServiceInternalApi serviceInternalApi = new ServiceInternalApi();// BaseURL
     @GetMapping("/type/{type}")
-    public ResponseEntity<String> getAnimalByAge(@PathVariable String type, @RequestParam(required = false) String age) {
+    @ResponseBody
+    public Animals getAnimalByAge(@PathVariable String type, @RequestParam(required = false) String age) {
         System.out.println("getAnimalByAge age= " + age);
         String result = serviceInternalApi.runService(type,age);
         System.out.println("CharacteristicsCat java getAnimalByAge result = "+ result);
         Gson gson = new Gson();
         Animals animals = gson.fromJson(result, Animals.class);
         System.out.println("animal = "+ gson.toJson(animals));
-        HttpHeaders httpHeaders = new HttpHeaders();
+//        HttpHeaders httpHeaders = new HttpHeaders();
         // Call for all Cats with Age
         //baby, young, adult, senior Accepts multiple values, e.g. age=baby,senior
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<String>("{\"test\": \"Hello with ResponseEntity\"}", httpHeaders, HttpStatus.OK);
+//        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return animals;
 //        return ResponseEntity.ok().headers(httpHeaders).body("hello");
     }
     @PostMapping("/hey")
